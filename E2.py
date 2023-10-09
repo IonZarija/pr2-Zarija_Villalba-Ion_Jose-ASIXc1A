a290e93266401cfbce07b90cbd2a6eb46ab03b3b
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-10-09 12:10:58 +0200

    P2

---
+++ E2.py
@@ -0,0 +1,12 @@
+""""
+Ion Zarija
+José Villalba
+09/10/23
+ASIXc1A M03 UF1 A2
+Programa que calcula el volumen de un aula
+"""
+bas = float(input("Introduzca la base del aula: "))
+alt = float(input("Ahora la altura: "))
+prof = float(input("Finalmente la profundidad: "))
+vol = bas*alt*prof
+print("El volumen del aula es: ", vol)
