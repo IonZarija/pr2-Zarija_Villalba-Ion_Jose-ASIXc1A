69dbb4f89448c4d0dd2cbe604cd85a3e17223aa3
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-10-09 12:25:58 +0200

    P2

--- E4.py
+++ E4.py
@@ -3,5 +3,12 @@ Ion Zarija
 José Villalba
 09/10/23
 ASIXc1A M03 UF1 A2
-Programa que pide el diámetro de una pizza y devuelve la superficie
+Programa que dice si tiene la edad suficiente para trabajar
 """
+edad = int(input("Introduzca su edad: "))
+if edad >= 16:
+    print("Tienes edad para trabajar")
+elif edad >= 65:
+    print("Disfrute su jubilación")
+else:
+    print("No tiene edad para trabajar")
