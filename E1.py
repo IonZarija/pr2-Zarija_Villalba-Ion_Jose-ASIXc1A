c94cdc9e1f4717692495df87a6d51bae32612d9b
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-10-09 11:59:19 +0200

    P2

---
+++ E1.py
@@ -0,0 +1,11 @@
+"""
+Ion Zarija
+José Villalba
+09/10/23
+ASIXc1A M03 UF1 A2
+Programa que pide el diámetro de una pizza y devuelve la superficie
+"""
+import math
+dPizza = int(input("Introduzca el diámetro de su pizza: "))
+sPizza = math.pi * (dPizza/2)**2
+print("La superfície de su pizza es: ")
