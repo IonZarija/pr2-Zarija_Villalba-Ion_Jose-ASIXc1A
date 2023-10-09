f7c08bd311b58ec5f19d5ca27e8d78672c93170f
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-10-09 12:34:39 +0200

    P2

--- E5.py
+++ E5.py
@@ -3,5 +3,7 @@ Ion Zarija
 José Villalba
 09/10/23
 ASIXc1A M03 UF1 A2
-Programa que pide el diámetro de una pizza y devuelve la superficie
+Programa que al intoducir una palabra, canvie las vocales por números ( a=1 )
 """
+pal = str(input("Introduzca una palabra: "))
+pal = pal.replace("a", "1")
