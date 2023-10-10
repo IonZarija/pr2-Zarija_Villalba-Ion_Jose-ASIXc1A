81b8d3c097224865b99f8582af98ac6f602da782
jose.villalba.7e7 <jose.villalba.7e7@itb.cat>
2023-10-10 11:36:48 +0200

    Ya está

--- E3.py
+++ E3.py
@@ -3,5 +3,17 @@ Ion Zarija
 José Villalba
 09/10/23
 ASIXc1A M03 UF1 A2
-Escriu un programa que lleg
+Escriu un programa que al introducir 5 números, muestra si el 5o está en el rango del 1o,2o y 3o,4o
 """
+list = []
+for x in range (5):
+    num = int(input("Introduce un valor"))
+    list.append(num)
+if (list[4] >= list[0] and list[4] <= list[1]) or (list[4] <= list[0] and list[4] >= list[1]):
+    print("El número está incluído en el primer rango")
+else:
+    print("El número no está incluído en el primer rango")
+if (list[4] >= list[2] and list[4] <= list[3]) or (list[4] <= list[2] and list[4] >= list[3]):
+    print("El número está incluído en el segundo rango")
+else:
+    print("El número no está incluído en el segundo rango")
